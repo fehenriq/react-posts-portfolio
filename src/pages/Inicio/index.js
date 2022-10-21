00ee474ed928205f9392ea3c0e22ef5892eaacfc
Felipe <lipe.h.r@gmail.com>
2022-10-21 10:08:54 -0300

    dinamizado dados do post e criado pagina notfound

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Post from 'components/Post'
+import PostCard from 'components/PostCard'
 import styles from './Inicio.module.css'
 import posts from 'json/posts.json'
 
@@ -8,7 +8,7 @@ export default function Inicio() {
     <ul className={styles.posts}>
       {posts.map((post) => (
         <li key={post.id}>
-          <Post post={post} />
+          <PostCard post={post} />
         </li>
       ))}
     </ul>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-import PaginaPadrao from "components/PaginaPadrao";
-import Rodape from "components/Rodape";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./components/Menu";
+import PaginaPadrao from "components/PaginaPadrao";
 import Inicio from "./pages/Inicio";
 import SobreMim from "./pages/SobreMim";
+import Post from "pages/Post";
+import Rodape from "components/Rodape";
+import NotFound from "pages/NotFound";
 
 export default function AppRoutes() {
   return (
@@ -12,9 +14,10 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
-          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
-        <Route path="*" element={<div>Página não encontrada.</div>} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Rodape />
     </BrowserRouter>
